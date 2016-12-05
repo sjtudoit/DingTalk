14468d041e9656ceb439d9e12a530118a4e47f8f
sjtudoit <songlian@SongLians-MacBook-Pro.local>
2016-12-05 22:51:45 +0800

    为access toke的获取加上一个缓存以免向钉钉服务器请求太频繁

--- lib/dingtalk/server/server.rb
+++ lib/dingtalk/server/server.rb
@@ -1,8 +1,11 @@
 require 'json'
+require 'rails'
 
 module Dingtalk
 
   class Server
+
+    ACCESS_TOKEN_CACHE_KEY = 'Dingtalk::Server::ACCESS_TOKEN_CACHE_KEY'
 
     # 构造函数
     # @param [String] corp_id 钉钉提供的ID
@@ -15,6 +18,26 @@ module Dingtalk
     # 查询AccessToken
     # @return [String] 直接返回token，如果失败则返回nil
     def query_access_token()
+      token = query_access_token_with_cache
+      if token.nil? || token.empty?
+        # 防止因为一些网络错误，缓存了空值，造成在缓存期内服务不可用
+        Rails.cache.delete(ACCESS_TOKEN_CACHE_KEY)
+        token = query_access_token_with_cache
+      end
+      token
+    end
+
+    # 查询AccessToken，带缓存
+    # @return [String] 直接返回token，如果失败则返回nil
+    def query_access_token_with_cache()
+      Rails.cache.fetch(ACCESS_TOKEN_CACHE_KEY, expires_in: 1.hours) do
+        query_access_token_with_direct
+      end
+    end
+
+    # 查询AccessToken，带缓存
+    # @return [String] 直接返回token，如果失败则返回nil
+    def query_access_token_with_direct()
       uri = URI(INTERFACE_URL + '/gettoken?' + 'corpid=' + @corp_id + '&corpsecret=' + @corp_secret)
       response = Net::HTTP.get(uri)
       result = JSON.parse(response)
--- spec/dingtalk_spec.rb
+++ spec/dingtalk_spec.rb
@@ -42,3 +42,29 @@ describe Dingtalk do
     expect(all_users).not_to be nil
   end
 end
+
+describe 'cache' do
+
+  Rails.cache = ActiveSupport::Cache.lookup_store(:memory_store)
+  Dingtalk.corpid = ENV['SJTUDOIT_DINGTALK_CORPID']
+  Dingtalk.corpsecret = ENV['SJTUDOIT_DINGTALK_CORPSECRET']
+  let(:server) { Dingtalk::Server.new(Dingtalk.corpid, Dingtalk.corpsecret) }
+
+  it 'cache access token correct' do
+    token_one = server.query_access_token
+    token_two = Rails.cache.fetch(Dingtalk::Server.const_get :ACCESS_TOKEN_CACHE_KEY)
+    expect(token_one).to eql(token_two)
+  end
+
+  it 'correct itself when cache nil' do
+    token_two = Rails.cache.write(Dingtalk::Server.const_get(:ACCESS_TOKEN_CACHE_KEY), nil)
+    token = server.query_access_token
+    expect(token).not_to be nil
+  end
+
+  it 'correct itself when cache empty' do
+    token_two = Rails.cache.write(Dingtalk::Server.const_get(:ACCESS_TOKEN_CACHE_KEY), '')
+    token = server.query_access_token
+    expect(token.empty?).not_to be true
+  end
+end
